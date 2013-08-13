2e65e07dcbc9600e59868c988900441216f7d6ff
L Lumbreras <llumbreras@gmail.com>
2013-08-12 19:33:20 -0700

    Create database table

--- db/migrate/20130813000056_create_posts.rb
+++ db/migrate/20130813000056_create_posts.rb
@@ -1,7 +1,13 @@
 class CreatePosts < ActiveRecord::Migration
-  def up
+  def self.up
+  	create_table :posts do |t|
+  		t.string :title
+  		t.text :body
+  		t.timestamps
+  	end
   end
 
-  def down
+  def self.down
+  	drop_table :posts
   end
 end

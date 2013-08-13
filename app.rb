db4574b53bf0ca7d7b35c7013f62488857b27625
L Lumbreras <llumbreras@gmail.com>
2013-08-13 14:51:35 -0700

    Include validations

--- app.rb
+++ app.rb
@@ -3,7 +3,11 @@
 
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'sinatra/flash'
+require 'sinatra/redirect_with_flash'
 require './environments'
+
+enable :sessions
 
 get "/" do
 	@posts = Post.order("created_at DESC")
@@ -11,11 +15,28 @@ get "/" do
 	erb :"posts/index"
 end
 
+post "/posts" do 
+	@post = Post.new(params[:id])
+	if @post.save
+		redirect "posts/#{@post.id}"
+	else
+		erb :"posts/create"
+	end
+end
+
+get "/posts/create" do 
+	@title = "Create Post"
+	@post = Post.new
+	erb :"posts/create"
+end
+
+
 get "/posts/:id" do 
 	@post = Post.find(params[:id])
 	@title = @post.title
 	erb :"posts/view"
 end
+
 
 helpers do 
 	def title
@@ -28,5 +49,7 @@ helpers do
 end
 
 class Post < ActiveRecord::Base
+	validates :title, presence: true, length: { minimum: 5 }
+	validates :body, presence: true
 end
 

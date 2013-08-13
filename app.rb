c8054a4f9514709d49288f6084a20449a3376627
L Lumbreras <llumbreras@gmail.com>
2013-08-12 19:44:08 -0700

    Add route and helpers

--- app.rb
+++ app.rb
@@ -5,6 +5,22 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require './environments'
 
+get "/" do
+	@posts = Post.order("created_at DESC")
+	@title = "Welcome"
+	erb :"posts/index"
+end
+
+helpers do 
+	def title
+		if @title
+			"#{@title}"
+		else
+			"Welcome"
+		end
+	end
+end
+
 class Post < ActiveRecord::Base
 end
 
